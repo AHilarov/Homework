8b5a54424ada444aadab6f3f0be421cf711d2cc5
AHilarov <ahilarov@yandex.ru>
2022-11-05 15:47:31 +0300

    добавил 3 и 4 задачи

---
+++ HW - Семинар 1 (13.10.2022)/Задача 3. Проверка чётности/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Эта программка будет проверять является ли введенное числo чётным. (Пожалуста, вводите только целые числа).");
+Restart:            //ссылка на откуда повторять
+    Console.Write("Введите число: ");
+string? input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+if (number % 2 == 0)
+{
+Console.WriteLine("Чётное");
+}
+else {Console.WriteLine("Нечетное");}
+
+
+// блок повтора
+Console.Write("Повторить? [Y/N] ");
+string? Answer = Console.ReadLine();
+if (Answer.ToLower() == "y") 
+{
+    goto Restart;
+}
+else Console.WriteLine("Пока!");
+
---
+++ HW - Семинар 1 (13.10.2022)/Задача 4. Все чётные до числа/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Эта программка будет на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N");
+Restart:            //ссылка на откуда повторять
+Console.Write("Введите целое положительное число больше 1: ");
+string? input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+int result = 2;
+//int count = 0;
+if (number > 1)
+{
+    while (result -2 < number - 3)
+// result - он же счётчик, но поскольку result начинается с 2, считать начинаем с result-2, то есть с 0
+// number делаем минус 3 (1+2). минус 1, чтобы если введенное число нечёт, то счётчик, чтобы не перескакивал, а минус 2 чтобы не висело в конце запятой. Мы его потом езё раз выведем
+    {
+    Console.Write($"{result}, ");
+    result = result + 2;
+    //count = count + 2;
+    }
+Console.WriteLine(result);
+}
+else Console.WriteLine("Вы ввели неправильное число");
+
+
+
+// блок повтора
+Console.Write("Повторить? [Y/N] ");
+string? Answer = Console.ReadLine();
+if (Answer.ToLower() == "y")
+{
+    goto Restart;
+}
+else Console.WriteLine("Пока!");
